26b483b1516cc68e50f3e10c5a6087ca073eec65
Karrick S. McDermott <karrick@gmail.com>
2023-10-27 08:52:58 -0400

    setup-python-mode

--- emacs/lisp/setup-python-mode.el
+++ emacs/lisp/setup-python-mode.el
@@ -3,45 +3,40 @@
 ;;; Code:
 
 (use-package emacs
-  :ensure t
   :hook (python-mode . (lambda ()
 						 (setq indent-tabs-mode nil
 							   tab-width 4))))
-
-(use-package python-black
-  :after python
-  ;; :demand t
-  :ensure t
-  :when (executable-find "black")
-  :hook (python-mode . python-black-on-save-mode-enable-dwim))
-
-(use-package lsp-pyright
-  :disabled
-  :ensure t
-  :hook (python-mode . (lambda ()
-						 (require 'lsp-pyright)
-						 (lsp))))		; or lsp-deferred
-
-(use-package pyvenv
-  :disabled
-  :ensure t
-  :hook (python-mode . pyvenv-mode)
-
-  :config
-  (when nil
-	(pyvenv-mode t)
-	(setq pyvenv-post-activation-hooks
-		  (list (lambda ()
-				  (setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python3")))))
-	(setq pyvenv-post-deactivation-hooks
-		  (list (lambda ()
-				  (setq python-shell-interpreter "python3"))))))
 
 (use-package elpy
   :disabled
   :ensure t
   :init
   (elpy-enable))
+
+(use-package python-black
+  :after python
+  :ensure t
+  :when (executable-find "black")
+  :hook (python-mode . python-black-on-save-mode-enable-dwim))
+
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+						 (require 'lsp-pyright)
+						 (lsp))))		; or lsp-deferred
+
+(use-package pyvenv
+  :ensure t
+  :hook (python-mode . pyvenv-mode)
+
+  :config
+  (when t (pyvenv-mode t))
+  (setq pyvenv-post-activation-hooks
+		(list (lambda ()
+				(setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python3")))))
+  (setq pyvenv-post-deactivation-hooks
+		(list (lambda ()
+				(setq python-shell-interpreter "python3")))))
 
 (provide 'setup-python-mode)
 
