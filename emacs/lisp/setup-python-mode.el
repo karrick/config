1aaf32d75f898c536b00deada95ed1fe125c3b8e
Karrick S. McDermott <karrick@gmail.com>
2023-11-18 17:48:57 -0500

    setup-python-mode
    Sort and tiny cleanups

--- emacs/lisp/setup-python-mode.el
+++ emacs/lisp/setup-python-mode.el
@@ -2,31 +2,30 @@
 
 ;;; Code:
 
+(use-package elpy
+  :ensure nil
+  :disabled
+  :init
+  (elpy-enable))
+
 (use-package emacs
   :hook (python-mode . (lambda ()
 						 (setq indent-tabs-mode nil
 							   tab-width 4))))
 
-(use-package elpy
-  :disabled
-  :ensure t
-  :init
-  (elpy-enable))
-
-(use-package python-black
-  :after python
-  :ensure t
-  :when (executable-find "black")
-  :hook (python-mode . python-black-on-save-mode-enable-dwim))
-
 (use-package lsp-pyright
+  :ensure nil
   :disabled
-  :ensure t
   :custom
   (lsp-pyright-python-executable-cmd "python3")
   :hook (python-mode . (lambda ()
 						 (require 'lsp-pyright)
 						 (lsp))))		; or lsp-deferred
+
+(use-package python-black
+  :after python
+  :when (executable-find "black")
+  :hook (python-mode . python-black-on-save-mode-enable-dwim))
 
 (use-package pyvenv
   :ensure t
@@ -39,7 +38,8 @@
 				(setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python3")))))
   (setq pyvenv-post-deactivation-hooks
 		(list (lambda ()
-				(setq python-shell-interpreter "python3")))))
+				(setq python-shell-interpreter "python3"))))
+  :ensure t)
 
 (provide 'setup-python-mode)
 
