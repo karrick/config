efa010e01e5c7adb31b39beaafc4f120dc627430
Karrick S. McDermott <karrick@gmail.com>
2024-04-14 07:00:55 -0400

    tree-sitter is available but not configured

--- emacs/lisp/setup-tree-sitter.el
+++ emacs/lisp/setup-tree-sitter.el
@@ -5,19 +5,23 @@
 ;;; Code:
 
 ;; tree-sitter is not yet configured properly.
-(when (and (functionp 'treesit-available-p)
-		   (treesit-available-p))
-  (require 'tree-sitter)
-  (require 'tree-sitter-langs)
-  (require 'tree-sitter-indent)
-  (global-tree-sitter-mode)
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-indent-mode)
-  (progn
-	(require 'tree-sitter-ispell)
-	(cond
-	 (nil (global-set-key (kbd "C-x C-s") #'tree-sitter-ispell-run-at-point))
-	 (nil (global-set-key (kbd "C-x C-s") #'tree-sitter-ispell-run-buffer)))))
+(if (and (functionp 'treesit-available-p)
+		 (treesit-available-p))
+	(if t
+		(message "tree-sitter is available but not configured")
+	  (require 'tree-sitter)
+	  (require 'tree-sitter-langs)
+	  (require 'tree-sitter-indent)
+	  (global-tree-sitter-mode)
+	  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+	  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-indent-mode)
+	  (progn
+		(require 'tree-sitter-ispell)
+		(cond
+		 (nil (global-set-key (kbd "C-x C-s") #'tree-sitter-ispell-run-at-point))
+		 (nil (global-set-key (kbd "C-x C-s") #'tree-sitter-ispell-run-buffer))))
+	  (message "tree-sitter is configured"))
+  (message "tree-sitter is *not* available"))
 
 (provide 'setup-tree-sitter)
 
