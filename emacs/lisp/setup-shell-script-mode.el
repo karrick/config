9b3047423b0992b8e89c63f5d32206d7349732ae
Karrick S. McDermott <karrick@gmail.com>
2024-08-12 09:32:13 -0400

    setup-shell-script-mode.el
    Introduction of Lisp file to configure Emacs for shell script programming.

---
+++ emacs/lisp/setup-shell-script-mode.el
@@ -0,0 +1,15 @@
+;;; setup-shell-script-mode -- customizations for shell programming
+
+;;; Commentary:
+
+;;; Code:
+
+;; go install mvdan.cc/sh/v3/cmd/shfmt@latest
+(when (executable-find "shfmt")
+  (use-package shfmt
+	:ensure t
+	:hook (sh-mode shfmt-on-save-mode)))
+
+(provide 'setup-shell-script-mode)
+
+;;; setup-shell-script-mode.el ends here
